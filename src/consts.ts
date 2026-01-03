9d31da08184fce5eb38d7f7fb9fa5a5586b8a2d5
aydelottejoe <j.a.aydelotte@gmail.com>
2026-01-03 16:27:44 +0100

    change consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "PCG";
+export const COMPANY_NAME = "Pleasanton Commercial Group";
+export const SITE_DESCRIPTION = "Business brokerage advisory services. Helping you buy or sell a business with confidence.";
+export const LINKEDIN_URL = "";
+export const EMAIL_ADDRESS = "info@pleasantoncg.com";
